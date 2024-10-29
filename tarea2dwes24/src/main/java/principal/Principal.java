32c0c4d5330f40777c33718c0efc96d662a291e5
ydr29913 <ydr29913@educastur.es>
2024-10-29 14:20:38 +0100

    Creacion de Menus

--- tarea2dwes24/src/main/java/principal/Principal.java
+++ tarea2dwes24/src/main/java/principal/Principal.java
@@ -20,7 +20,7 @@ public class Principal {
 	public static void main(String[] args) {
 		Principal p = new Principal();
 		//Sirve para llamar al metodo para mostrar el menu
-		//p.menu();
+		p.menu();
 		
 		Scanner sc = new Scanner(System.in);
 		
@@ -82,5 +82,137 @@ public class Principal {
 			e.printStackTrace();
 		}
 	}
+	
+	
+	
+	//Sirve para mostrar un menu de opciones por pantalla
+	public void menu() {
+		Scanner sc = new Scanner(System.in);
+		
+		int opcion = 0;
 
+        do {
+            try {
+                System.out.println("\n\n\n\t\t\tGESTION DE VIVERO\n");
+                System.out.println("\t\t\t\t1 - LOGIN");
+                System.out.println("\t\t\t\t2 - AÑADIR PLANTAS");
+                System.out.println("\t\t\t\t3 - ..............");
+                System.out.println("\t\t\t\t4 - ..............");
+                System.out.println("\t\t\t\t5 - ..............");
+                System.out.println("\t\t\t\t9 - SALIR");
+
+                opcion = sc.nextInt();
+                switch (opcion) {
+                	case 1:
+                		logearse();
+                		break;
+                	case 2:
+                        añadirPlanta();
+                        break;
+                    case 9:
+                        break;
+                }
+            } catch (InputMismatchException e) {
+                System.out.println("Debes introducir un número entero");
+                sc.nextLine();
+            }
+        } while (opcion != 9);
+	}
+	
+	
+	
+	//Sirve para logearse en la aplicacion
+	public void logearse() {
+		Scanner sc = new Scanner(System.in);
+		
+		int opcion = 0;
+
+        do {
+            try {
+                System.out.println("\n\n\n\t\t\tLOGIN\n");
+                System.out.println("\t\t\t\t1 - INVITADO");
+                System.out.println("\t\t\t\t2 - PERSONAL");
+                System.out.println("\t\t\t\t3 - ADMINISTRADOR GENERAL");
+                System.out.println("\t\t\t\t9 - SALIR");
+
+                opcion = sc.nextInt();
+                switch (opcion) {
+                	case 1:
+                		invitado();
+                		break;
+                	case 2:
+                        //personal();
+                        break;
+                	case 3:
+                		//administradorGeneral();
+                    case 9:
+                        break;
+                }
+            } catch (InputMismatchException e) {
+                System.out.println("Debes introducir un número entero");
+                sc.nextLine();
+            }
+        } while (opcion != 9);
+	}
+	
+	
+	//Menu del usuario invitado
+	public void invitado() {
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("\n\tIntroduce el nombre del usuario: ");
+			String usuario = sc.nextLine();
+		System.out.println("\n\tIntroduce la contraseña del usuario: ");
+			String password = sc.nextLine();
+		
+		
+		
+		int opcion = 0;
+
+        do {
+            try {
+                System.out.println("\n\n\n\t\t\tGESTION VIVERO (INVITADO)\n");
+                System.out.println("\t\t\t\t1 - ........");
+                System.out.println("\t\t\t\t2 - ........");
+                System.out.println("\t\t\t\t3 - ........");
+                System.out.println("\t\t\t\t9 - SALIR");
+
+                opcion = sc.nextInt();
+                switch (opcion) {
+                	case 1:
+                		//........();
+                		break;
+                	case 2:
+                        //........();
+                        break;
+                	case 3:
+                		//........();
+                    case 9:
+                        break;
+                }
+            } catch (InputMismatchException e) {
+                System.out.println("Debes introducir un número entero");
+                sc.nextLine();
+            }
+        } while (opcion != 9);
+	}
+		
+	
+	
+	//Sirve para añadir una nueva planta a la base de datos
+	public void añadirPlanta() {
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Dame el codigo de la nueva planta: ");
+		String codigo = sc.nextLine().trim().toUpperCase();
+		
+		System.out.println("Dame el nombre comun de la planta: ");
+		String nombre_comun = sc.nextLine();
+		
+		System.out.println("Dame el nombre cientifico de la planta: ");
+		String nombre_cientifico = sc.nextLine();
+		
+		Planta nueva = new Planta(codigo, nombre_comun, nombre_cientifico);
+	}
+	
 }
